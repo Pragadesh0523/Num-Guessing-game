d66604a12c624df19497ffd190b91a6490815e22
Pragadesh0523 <pragadeshs23@gmail.com>
2026-06-25 10:20:32 +0530

    Add files via upload
    secret number is static.
    no limit for number of guesses.

---
+++ num guess ver1.py
@@ -0,0 +1,11 @@
+num=33
+while True :
+    x = int(input("Enter a number: "))
+    if x>num:
+        print("My number is lesser than ",x)
+    elif x<num:
+        print("My number is greater than ",x)
+    else:
+        print("The secret number is: ",x)
+        print("You Won!!")
+        break
